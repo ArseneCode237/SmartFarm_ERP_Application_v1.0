5749b280cc2c89ad35a9a427b4f37396b4577429
Giovanni-Romuald <giovannikamdem7@gmail.com>
2026-07-14 17:43:19 +0100

    local

--- database/create-ferme-intelligente.sql
+++ database/create-ferme-intelligente.sql
@@ -1,5 +1,4 @@
--- Executez ce script avec un compte PostgreSQL ayant le droit CREATEDB.
--- Exemple : psql -U postgres -d postgres -f database/create-ferme-intelligente.sql
+
 
 SELECT 'CREATE DATABASE ferme_intelligente'
 WHERE NOT EXISTS (
