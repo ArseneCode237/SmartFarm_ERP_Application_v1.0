6ad0cd631a88fea47c1920bc6663a835f1788ab5
ArseneCode237 <arseneshinra@gmail.com>
2026-08-14 16:57:40 +0100

    refactor(ProviderMapper) : ajuster le modèle de composant et corriger le saut de ligne final

--- stocks/src/main/java/com/reseau_partage/stocks/mapper/FournisseurMapper.java
+++ stocks/src/main/java/com/reseau_partage/stocks/mapper/FournisseurMapper.java
@@ -5,7 +5,7 @@ import com.reseau_partage.core.entities.Fournisseur;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+@Mapper(componentModel = "default", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface FournisseurMapper {
 
     FournisseurResponse toResponse(Fournisseur entity);
